97a9fd993cc9ef41f8e6b12cd676a355b4eac804
rjk <rjk>
2019-05-06 14:04:28 +1000

    Added recursive functions for multiple days, plus helper functions for weekday / weekend.

--- R/DateTime.R
+++ R/DateTime.R
@@ -4,7 +4,7 @@
 #' @return The previous weekday
 #' @examples
 #' getPreviousWeekday(today())
-#' #' @export
+#' @export
 getPreviousWeekday <- function(date) {
 
   dow <- wday(date, week_start = 6)
@@ -19,11 +19,91 @@ getPreviousWeekday <- function(date) {
 #' @return The next weekday
 #' @examples
 #' getNextWeekday(today())
-#' #' @export
+#' @export
 getNextWeekday <- function(date) {
 
   dow <- wday(date, week_start = 5)
   offset <- if_else(dow > 3, 1, abs(dow-4))
 
   return (date + offset)
+}
+
+#' Return the nth weekday from now
+#'
+#' @param date - the date to offset
+#' @param  n - the number of days to offset (a negative n means reverse the meaning of the forward parameter)
+#' @param  forward - forward or backward offset
+#' @return The resultant weekday
+#' @examples
+#' offsetByWeekdays(today(), 4, forward = FALSE)
+#' offsetByWeekdays(today(), -4)
+#' @export
+offsetByWeekdays <- function(date, n, forward = TRUE) {
+
+  # Handle negative n
+  if (n < 0) {
+    n <- -1 * n
+    forward <- !forward
+  }
+
+  dates <- .iterateOverWeekdays(date, n, forward)
+
+  return (tail(dates,1))
+}
+
+
+#' Return a list of the n adjacent weekdays
+#'
+#' @param date - the date to offset
+#' @param  n - the number of days to offset (a negative n means reverse the meaning of the forward parameter)
+#' @param  forward - forward or backward offset
+#' @return The next weekday(s)
+#' @examples
+#' getAdjacentWeekdays(today(), 4, forward = FALSE)
+#' getAdjacentWeekdays(today(), -4)
+#' @export
+getAdjacentWeekdays <- function(date, n, forward = TRUE) {
+
+  # Handle negative n
+  if (n < 0) {
+    n <- -1 * n
+    forward <- !forward
+  }
+
+  dates <- .iterateOverWeekdays(date, n, forward)
+
+  return (dates[-1])
+}
+
+# Internal method to do the actual iteration.
+.iterateOverWeekdays <- function(date, n, forward = TRUE) {
+
+  if (n == 0)
+    return (date)
+
+  newDate <- ifelse(forward, getNextWeekday(tail(date,1)), getPreviousWeekday(tail(date, 1)))
+
+  return (c(date, .iterateOverWeekdays(newDate, n-1, forward)))
+}
+
+#' Check if a date falls on a weekend
+#'
+#' @param date - the date(s) to test
+#' @return A boolean (or list of booleans)
+#' @examples
+#' isWeekend(c(ymd("20190505"), ymd("20190506")))
+#' @export
+isWeekend <- function(date) {
+  return (!isWeekday(date))
+}
+
+#' Check if a date falls on a weekday
+#'
+#' @param date - the date(s) to test
+#' @return A boolean (or list of booleans)
+#' @examples
+#' isWeekday(c(ymd("20190505"), ymd("20190506")))
+#' @export
+isWeekday <- function(date) {
+  return (wday(date, week_start = 1) < 6)
 }
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(RichUtils)
+
+test_check("RichUtils")
